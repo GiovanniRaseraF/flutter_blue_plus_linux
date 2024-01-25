fb6952b9e75ecd7c09fc2576b2fcb12a4b7ab620
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2024-01-25 17:16:55 +0100

    simpleble: compiles

--- dbuschanneltest/linux/my_application.h
+++ dbuschanneltest/linux/my_application.h
@@ -3,12 +3,9 @@
 
 #include <gtk/gtk.h>
 #include <math.h>
-#include <dbus/dbus.h>
 #include <boost/array.hpp>
 #include <boost/filesystem.hpp>
 #include <iostream>
-
-#include "libs/Path.h"
 
 G_DECLARE_FINAL_TYPE(MyApplication, my_application, MY, APPLICATION,
                      GtkApplication)
@@ -27,60 +24,12 @@ struct MyDBus
   int count = 0;
   MyDBus()
   {
-    int c = SimpleDBus::Path::count_elements("/ciao/come/va");
-    std::cout << c << std::endl;
     boost::array<std::string, 10> myarray;
     myarray[5] = "hello";
     std::cout << myarray[5] << std::endl;
 
     boost::filesystem::path p{};
     std::cout << p << std::endl;
-
-    //org.freedesktop.DBus.Properties
-    DBusError dbus_error;
-    DBusConnection * dbus_conn = nullptr;
-    DBusMessage * dbus_msg = nullptr;
-    DBusMessage * dbus_reply = nullptr;
-    const char * dbus_result = nullptr;
-
-    // Initialize D-Bus error
-    ::dbus_error_init(&dbus_error);
-
-    // Connect to D-Bus
-    if ( nullptr == (dbus_conn = ::dbus_bus_get(DBUS_BUS_SYSTEM, &dbus_error)) ) {
-        ::perror(dbus_error.name);
-        ::perror(dbus_error.message);
-
-    // Compose remote procedure call
-    } else if ( nullptr == (dbus_msg = ::dbus_message_new_method_call("org.bluez", "/org/bluez/hci0", "org.freedesktop.DBus.Properties", "Get")) ) {
-        ::dbus_connection_unref(dbus_conn);
-        ::perror("ERROR: ::dbus_message_new_method_call - Unable to allocate memory for the message!");
-
-    // Invoke remote procedure call, block for response
-    } else if ( nullptr == (dbus_reply = ::dbus_connection_send_with_reply_and_block(dbus_conn, dbus_msg, DBUS_TIMEOUT_USE_DEFAULT, &dbus_error)) ) {
-        ::dbus_message_unref(dbus_msg);
-        ::dbus_connection_unref(dbus_conn);
-        ::perror(dbus_error.name);
-        ::perror(dbus_error.message);
-
-    // Parse response
-    } else if ( !::dbus_message_get_args(dbus_reply, &dbus_error, DBUS_TYPE_STRING, &dbus_result, DBUS_TYPE_INVALID) ) {
-        ::dbus_message_unref(dbus_msg);
-        ::dbus_message_unref(dbus_reply);
-        ::dbus_connection_unref(dbus_conn);
-        ::perror(dbus_error.name);
-        ::perror(dbus_error.message);
-
-    // Work with the results of the remote procedure call
-    } else {
-        std::cout << "Connected to D-Bus as \"" << ::dbus_bus_get_unique_name(dbus_conn) << "\"." << std::endl;
-        std::cout << "Introspection Result:" << std::endl;
-        std::cout << std::endl << dbus_result << std::endl << std::endl;
-        ::dbus_message_unref(dbus_msg);
-        ::dbus_message_unref(dbus_reply);
-
-        ::dbus_connection_unref(dbus_conn);
-    }
 
   }
 };
